396a32b990a069930819688a435b4197e49823b4
Arzhang <arjang.ap3@gmail.com>
2022-11-11 13:18:49 +0330

    Using lateinit

--- src/com/bignerdranch/nyetHack/NyetHack.kt
+++ src/com/bignerdranch/nyetHack/NyetHack.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.nyetHack
 
-val player = Player("Arzhang","Jacksonvillie")
+lateinit var player: Player
 fun main() {
+    narrate("Welcome to NyetHack!")
+    val playerName = promptHeroName()
+    player = Player(playerName)
     //change narrator mood
     val mortality = if(player.isImmortal) "an Immortal" else "a mortal"
     narrate("${player.name} of ${player.hometown}, ${player.title}, heads to the town square." +
