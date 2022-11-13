739976490b716af01cbeea3e1f143d602252e78f
Arzhang <arjang.ap3@gmail.com>
2022-11-13 11:02:46 +0330

    Adding a new features.

--- src/com/bignerdranch/nyetHack/TownSquare.kt
+++ src/com/bignerdranch/nyetHack/TownSquare.kt
@@ -2,8 +2,14 @@ package com.bignerdranch.nyetHack
 
 class TownSquare : Room(name = "The Town Square") {
     override val status = "Bustling"
+    private val bellSound = "GWONG"
 
     override fun enterRoom() {
         narrate("The Villagers rally and cheer as the hero enters.")
+        ringBell()
+    }
+
+    fun ringBell() {
+        narrate("The bell tower announces the hero's presence: $bellSound)
     }
 }
