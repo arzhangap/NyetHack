99c5fc17bdd8041b490a4c0eec115b18d7534939
Arzhang <arjang.ap3@gmail.com>
2022-11-17 13:05:01 +0330

    moving code from NyetHack to Game

--- src/com/bignerdranch/nyetHack/Game.kt
+++ src/com/bignerdranch/nyetHack/Game.kt
@@ -1,7 +1,22 @@
 package com.bignerdranch.nyetHack
 
 object Game {
+    private var currentRoom: Room = TownSquare()
+
     init {
         narrate("Welcome, adventurer")
+        val mortality = if(player.isImmortal) "an Immortal" else "a mortal"
+        narrate("${player.name}, $mortality, has ${player.healthPoints} health points.")
+    }
+
+    fun play() {
+        while (true) {
+            // Play NyetHack
+            narrate("${player.name} of ${player.hometown}, ${player.title}, is in ${currentRoom.description()}")
+            currentRoom.enterRoom()
+
+            print("> Enter yout command:  ")
+            println("Last command: ${readLine()}")
+        }
     }
 }
