98d0098b23a1e5f7ceb09675b2479bea3bd61bf9
Arzhang <arjang.ap3@gmail.com>
2022-11-27 15:34:42 +0330

    override plus operator

--- src/com/bignerdranch/nyetHack/Navigation.kt
+++ src/com/bignerdranch/nyetHack/Navigation.kt
@@ -1,3 +1,19 @@
 package com.bignerdranch.nyetHack
 
-data class Coordinate(val x: Int, val y: Int)
+data class Coordinate(val x: Int, val y: Int) {
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
+}
+enum class Direction(
+    private val directionCoordinate: Coordinate
+) {
+    North(Coordinate(0,-1)),
+    East(Coordinate(1,0)),
+    South(Coordinate(0,1)),
+    West(Coordinate(-1,0));
+
+    fun updateCoordinate(coordinate: Coordinate) = // Coordinate(
+//        x = coordinate.x + directionCoordinate.x,
+//        y = coordinate.y + directionCoordinate.y )
+    coordinate + directionCoordinate
+
+}
