7ecc58cd698c17c1aa6da4c5f783e6695a21ae62
Arzhang <arjang.ap3@gmail.com>
2022-12-29 12:24:04 +0330

    Expand Loot

--- src/com/bignerdranch/nyetHack/Loot.kt
+++ src/com/bignerdranch/nyetHack/Loot.kt
@@ -1,13 +1,18 @@
 package com.bignerdranch.nyetHack
-
 abstract class Loot {
     abstract val name: String
+}
+
+class DropOffBox<in T> where T: Loot, T : Sellable {
+    fun sellLoot(sellableLoot: T) : Int{
+        return (sellableLoot.value * 0.7).toInt()
+    }
 }
 
 interface Sellable {
     val value: Int
 }
-class LootBox<T: Loot>(var contents: T) {
+class LootBox<out T: Loot>(val contents: T) {
     var isOpen = false
         private set
 
@@ -17,10 +22,17 @@ class LootBox<T: Loot>(var contents: T) {
     }
 }
 
+abstract class Hat : Sellable, Loot()
+
 class Fedora(
     override val name: String,
     override val value: Int
-) : Loot(), Sellable
+) : Hat()
+
+class Fez(
+    override val name: String,
+    override val value: Int
+) : Hat()
 
 class Gemstones(
     override val value: Int
