22f0cfb3e055986e97ea8e0a876ffa44cae6046b
Arzhang <arjang.ap3@gmail.com>
2022-12-29 12:43:31 +0330

    Randomizing loot

--- src/com/bignerdranch/nyetHack/Loot.kt
+++ src/com/bignerdranch/nyetHack/Loot.kt
@@ -1,4 +1,8 @@
 package com.bignerdranch.nyetHack
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
 abstract class Loot {
     abstract val name: String
 }
@@ -19,6 +23,16 @@ class LootBox<out T: Loot>(val contents: T) {
     fun takeLoot() : T? {
         return contents.takeIf { !isOpen }.
                 also { isOpen = true }
+    }
+    companion object {
+        fun random(): LootBox<Loot> = LootBox(contents =
+        when (Random.nextInt(1..100)) {
+            in 1..5 -> Fez("fez of immaculate style",150)
+            in 6..10 -> Fez("fedora of knowledge",125)
+            in 11..15 -> Fez("stunning teal fedora",75)
+            in 16..30 -> Fez("ordinary fedora",10)
+            else -> Gemstones(Random.nextInt(50..100))
+        })
     }
 }
 
